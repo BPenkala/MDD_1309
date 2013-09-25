6f56b4d5724a062a71cac735ba5e49f32c360453
Bryan Penkala <bpenkala@fullsail.edu>
2013-09-24 23:40:02 -0400

    Branding
    Added some unique branding/styling.  Added Comments, various code
    updates.

--- searchagram_app/js/searchagram.js
+++ searchagram_app/js/searchagram.js
@@ -1,5 +1,6 @@
 var searchagram = angular.module(
 		'searchagram', []
+		//config block with dependencies
 	).config(
 		['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) {
 			$routeProvider
@@ -11,16 +12,15 @@ var searchagram = angular.module(
 			var api = 'https://api.instagram.com/v1/tags/%tag%/media/recent?access_token=46913576.5a81226.8e0ba9c992134d9bbab575fb4479bcad&callback=JSON_CALLBACK',
 				newReq, refreshApi;
 
+			// Gets images
 			$scope.fetchImages = function() {
-				
-				$scope.loadingClass = 'loading';
-				$scope.imgCurrent = 0;
-
+				// checking for new hashtag entered by user
 				if ( ! $route.current )
 					$location.path( '/tag/' + $scope.tag );
 				else if ( angular.isDefined( $route.current.params.tag ) )
 					$scope.tag = $route.current.params.tag;
 
+				//instagram api call using the tag entered by user
 				$http.jsonp( 
 					api.replace( '%tag%', $scope.tag )
 				).success( function( data ) {
@@ -34,16 +34,18 @@ var searchagram = angular.module(
 						
 				}).error( function() {
 					delete $scope.loadingClass;
-					refreshApi = $timeout( $scope.fetchImages, 2000 );
+					refreshApi = $timeout( $scope.fetchImages, 1000 );
 				});
 			}
 
+			//checking for new tag being entered
 			$scope.tagChange = function() {
 				$location.path( '/tag/' + $scope.tag );
 
 				if ( newReq )
 					$timeout.cancel( newReq );
 
+			
 				newReq = $timeout( function() {
 					$scope.fetchImages();
 					$timeout.cancel( newReq );
